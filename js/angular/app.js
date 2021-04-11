060d55d07fb4913f7bf25e560fbeca70403c4620
Yoseph Alabdulwahab <10735559+yoseph1998@users.noreply.github.com>
2021-04-11 01:50:40 -0500

    Added Basic Structure

---
+++ js/angular/app.js
@@ -0,0 +1,25 @@
+function getCurrentView() {
+    let qsArr = window.location.href.split('#');
+
+    if(qsArr.length >= 2 ) {
+        return qsArr[qsArr.length-1];
+    }
+    return "project-overview.html";
+}
+console.log(getCurrentView());
+
+var app = angular.module('myApp', ['ngAnimate']);
+
+app.controller('myCtrl', function($scope) {
+    $scope.viewSrc = getCurrentView();
+    $scope.scrollTo = function(id) {
+        var element = document.getElementById(id);
+        if(element != null) {
+            element.scrollIntoView({ behavior: "smooth" });
+        }
+    }
+    window.onhashchange = function() { 
+        $scope.viewSrc = getCurrentView();
+    };
+});
+
